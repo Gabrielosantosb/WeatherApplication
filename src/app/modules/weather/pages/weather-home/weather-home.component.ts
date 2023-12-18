8baa7109ae595fff952a39e2af61c28f2585204e
gabrielosantosb <email@email.com>
2023-12-18 13:24:33 -0300

    Adicionando validacao

--- src/app/modules/weather/pages/weather-home/weather-home.component.ts
+++ src/app/modules/weather/pages/weather-home/weather-home.component.ts
@@ -13,9 +13,11 @@ export class WeatherHomeComponent implements OnInit, OnDestroy {
   private readonly destroy$: Subject<void> = new Subject()
   initialCity = "Brasilia"
   weatherData !: WeatherInterface
+  setError = ""
   searchIcon = faMagnifyingGlass
 
-  constructor(private weatherService: WeatherService) {}
+  constructor(private weatherService: WeatherService) {
+  }
 
   ngOnInit(): void {
     this.getWeatherData(this.initialCity)
@@ -25,17 +27,32 @@ export class WeatherHomeComponent implements OnInit, OnDestroy {
     this.weatherService.getWeatherData(city).pipe(
       takeUntil(this.destroy$)
     ).subscribe({
-        next: (response) => {
-          this.weatherData = response;
-          console.log(response)
-        }
+      next: (response) => {
+        this.weatherData = response;
+        this.setError = "";
       },
-    )
+      error: (error) => {
+        console.error('Error fetching weather data:', error);
+        this.setError = 'Erro ao obter dados de clima.';
+      }
+    });
   }
 
-  onSubmit(): void{
-    this.getWeatherData(this.initialCity);
-    this.initialCity = '';
+  onSubmit(): void {
+    this.isError()
+    if (this.initialCity.trim() !== "") {
+      this.getWeatherData(this.initialCity);
+      this.initialCity = '';
+    }
+  }
+
+  isError(): void {
+    if (this.initialCity.trim() == "") {
+      this.setError = "Insira uma cidade no campo"
+    } else {
+
+      this.setError = "";
+    }
   }
 
   ngOnDestroy(): void {
