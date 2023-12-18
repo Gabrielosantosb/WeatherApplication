9fb171b467a95606ed0e1f3e8c4fc6e9230a779d
gabrielosantosb <email@email.com>
2023-12-18 12:27:45 -0300

    WeatherAppCompletado

--- src/app/modules/weather/components/weather-card/weather-card.component.ts
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -7,7 +7,7 @@ import {faDroplet, faTemperatureHigh, faTemperatureLow, faWind} from "@fortaweso
   templateUrl: './weather-card.component.html',
   styleUrls: ['./weather-card.component.scss']
 })
-export class WeatherChartComponent implements OnInit{
+export class WeatherChartComponent implements OnInit {
   // Entrada de dados
   @Input() weatherDataInput!: WeatherInterface;
   Temperature!: number;
@@ -15,14 +15,17 @@ export class WeatherChartComponent implements OnInit{
   MaxTemperature !: number;
 
   minTemperatureIcon = faTemperatureLow;
-  maxTemperaturaIcon = faTemperatureHigh;
+  maxTemperatureIcon = faTemperatureHigh;
   humidityIcon = faDroplet;
   windIcon = faWind;
 
   ngOnInit(): void {
-    this.Temperature = Number(this.weatherDataInput?.main.temp.toFixed(0).toString().substring(0, 2));
-    this.MinTemperature = Number(this.weatherDataInput?.main.temp_min.toFixed(0).toString().substring(0, 2));
-    this.MaxTemperature = Number(this.weatherDataInput?.main.temp_max.toFixed(0).toString().substring(0, 2));
+    this.updateWeatherData(this.weatherDataInput);
+  }
+
+  updateWeatherData(newData: WeatherInterface): void {
+    this.Temperature = Number(newData?.main.temp.toFixed(0).toString().substring(0, 2));
+    this.MinTemperature = Number(newData?.main.temp_min.toFixed(0).toString().substring(0, 2));
+    this.MaxTemperature = Number(newData?.main.temp_max.toFixed(0).toString().substring(0, 2));
   }
 }
-
